679a45eb60581d28e8ffad57973e7b90f50abfb8
Abhilasha Jha <abhilashajha41@gmail.com>
2020-06-28 18:55:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.get('/', (req, res) => res.send('Hello World!'));
-
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.post('/upload-image', (req, res) => {
     let imgAsBase64 = req.body.image; 
     require('fs').writeFileSync('image.png', imgAsBase64, 'base64', (err) => {
