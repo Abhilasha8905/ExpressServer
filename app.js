0e11d9c561710b3acdca4fec5c4d78ce1660a8cc
Abhilasha Jha <abhilashajha41@gmail.com>
2020-06-28 15:42:43 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const request = require('request');
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 var bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
